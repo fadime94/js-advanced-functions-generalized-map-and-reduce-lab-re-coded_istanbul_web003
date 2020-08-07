17ba13475b08f4770cd9618f7a299578c8dbc6cd
fadime94 <fadime.ozdemir11@gmail.com>
2020-08-07 20:33:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Add your functions here
+/* function mapToNegativize(sourceArray){
+    const negativeNums=[];
+    for(const el of sourceArray){
+        negativeNums.push(-1*el);
+    }
+    return negativeNums;
+} 
+
+function mapToDouble(sourceArray){
+    const double=[];
+    for(const el of sourceArray){
+        double.push(2*el);
+    }
+    return double;
+}
+*/
+
+function map(sourceArray,doSomeWork){
+    const newArr=[];
+    for(const el of sourceArray){
+       newArr.push(doSomeWork(el));
+    }
+    return newArr;
+}
+
+/*
+function reduceToTotal(sourceArray,acc=sourceArray[0]){
+    let total=startingPoint;
+    for(const el of sourceArray){
+        total+=el;
+    }
+    return total;
+}
+function reduceToAllTrue(sourceArray){
+    let result= sourceArray.every(el=> el? true:false);
+    return result;
+} 
+arr.reduce(function(acc,el){} , startValue)
+*/
+
+ function reduce(sourceArray,doSomeWork, startingPoint){
+    let accumulator;
+    if (startingPoint !== undefined) {
+        accumulator = startingPoint;
+        for (const el of sourceArray) {
+            accumulator = doSomeWork(accumulator, el);
+        }
+    } else {
+        accumulator = sourceArray[0]; 
+        sourceArray= sourceArray.slice(1);
+        for (const el of sourceArray) {
+            accumulator = doSomeWork(accumulator, el);
+        }
+    }
+    return accumulator;
+} 
+
+/* function reduceToTotal(sourceArray,acc=sourceArray[0]){
+    let total=acc;
+    for(const el of sourceArray){
+        total+=el;
+    }
+    return total;
+} */
